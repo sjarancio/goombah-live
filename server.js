d72eea7efa07a948b46bd08d3fc97f24c266981e
sjarancio <sjarancio@gmail.com>
2022-01-05 15:56:06 -0500

    server file

--- server.js
+++ server.js
@@ -27,9 +27,32 @@ app.get("/api/memberAdd", (req, res) => {
         })
 });
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/build/index.html'))
-})
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname + '/client/build/index.html'))
+// })
+
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+}
+
+const whitelist = ['http://localhost:3000', 'http://localhost:9001']
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin)
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable")
+      callback(null, true)
+    } else {
+      console.log("Origin rejected")
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
 
 const port = process.env.PORT || 9001;
 app.listen(port);
